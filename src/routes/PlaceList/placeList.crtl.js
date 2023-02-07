4b41b32a3a7083695354b711fc24b02ba40e4d0a
younhwan97 <younhwan0903@gmail.com>
2023-02-07 15:56:15 +0900

    [Add] 임시 데이터 추가

--- src/routes/PlaceList/placeList.crtl.js
+++ src/routes/PlaceList/placeList.crtl.js
@@ -1,7 +1,7 @@
 const placeList = {
     get: (req, res) => {
         // 타입 정보를 얻어온다
-        let type = req.body.type || 0
+        // let type = req.body.type || 0
 
         // 쿼리 생성
         let query = "SELECT * FROM places type = ?;"
@@ -9,7 +9,93 @@ const placeList = {
         // 요청
         // connect..
 
-        return res.send("나영츄는 바보이다.")
+        return res.json(
+            {
+                "count": 10,
+                "items": [
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                    {
+                        "id": 1,
+                        "name": "다원",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 100,
+                        "likeCount": 100,
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                    },
+                ]
+            }
+        )
     }
 }
 
