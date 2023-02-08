05967bd735827f4b90c7599c0841024fc65971dd
younhwan97 <younhwan0903@gmail.com>
2023-02-08 18:48:13 +0900

    [Update] 임시 데이터 수정

--- src/routes/PlaceList/placeList.crtl.js
+++ src/routes/PlaceList/placeList.crtl.js
@@ -1,9 +1,6 @@
 const placeList = {
 
     type0: (req, res) => {
-        // 타입 정보를 얻어온다
-        let type = req.body.type || 0
-
         // 쿼리 생성
         let query = "SELECT * FROM places type = ?;"
 
@@ -20,88 +17,46 @@ const placeList = {
                         "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
                         "reviewCount": 100,
                         "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살",
+                        "filter": "맛집"
                     },
                     {
-                        "id": 1,
-                        "name": "다원",
+                        "id": 2,
+                        "name": "드림하이빌",
+                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
+                        "reviewCount": 9999,
+                        "likeCount": 9999,
+                        "tags": "#나영츄 #바부 #크크",
+                        "filter": "맛집"
+                    },
+                    {
+                        "id": 3,
+                        "name": "으뜸마루",
                         "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
                         "reviewCount": 100,
                         "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살",
+                        "filter": "술집"
                     },
                     {
-                        "id": 1,
-                        "name": "다원",
+                        "id": 4,
+                        "name": "알촌",
                         "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
                         "reviewCount": 100,
                         "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
-                    },
-                    {
-                        "id": 1,
-                        "name": "다원",
-                        "image": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MjlfMjY0%2FMDAxNjYxNzUzODUxNjQ5.wfRsi5D0fW3qF0jMLlzHfjt3H57luONOVGaEik9gieYg.pKzR9pN_MCvWKJzW4SZP_HMZputo5IQ3yYrHL922LXsg.JPEG.ajfkgkwl153%2FIMG_1527.jpg&type=sc960_832",
-                        "reviewCount": 100,
-                        "likeCount": 100,
-                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살"
+                        "tags": "#가성비 #맛집 #볶음밥 #삼겹살",
+                        "filter": "맛집"
                     },
                 ]
             }
         )
     },
 
-    type1: (req, res) => {},
+    type1: (req, res) => {
+    },
 
-    type2: (req, res) => {}
+    type2: (req, res) => {
+    }
 }
 
 module.exports = {
