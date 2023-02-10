1e0fdf689ef2a89c604613346c045c9e12e602a3
younhwan97 <younhwan0903@gmail.com>
2023-02-10 18:51:31 +0900

    [Add] .

--- src/routes/PlaceDatail/placeDatail.ctrl.js
+++ src/routes/PlaceDatail/placeDatail.ctrl.js
@@ -2,6 +2,10 @@ const placeDetail = {
 
     get: (req, res) => {
         // 타입 정보를 얻어온다
+        let placeId = req.url
+        placeId = placeId.substring(1)
+
+        console.log(placeId)
 
     }
 }
