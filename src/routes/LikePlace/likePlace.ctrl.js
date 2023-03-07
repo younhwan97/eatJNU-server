97d0bd8b28f6ec33c99cce0319f38a5147d56ae9
younhwan97 <younhwan0903@gmail.com>
2023-03-08 02:23:24 +0900

    [Refactor] .

--- src/routes/LikePlace/likePlace.ctrl.js
+++ src/routes/LikePlace/likePlace.ctrl.js
@@ -39,12 +39,21 @@ const likePlace = {
         let query_like = "INSERT IGNORE INTO like_store(user_id, store_id) VALUES (?, ?);"
         query_like = mysql.format(query_like, [userId, placeId])
 
-        let query_store = "UPDATE store SET like_count = like_count + 1  WHERE store_id = ?;"
-        query_store = mysql.format(query_store, placeId)
+        let query_like2 = "SELECT * FROM like_store WHERE store_id = ?;"
+        query_like2 = mysql.format(query_like2, placeId)
 
         // DB 요청
-        req.app.get('dbConnection').query(query_like + query_store, (err, result) => {
+        req.app.get('dbConnection').query(query_like + query_like2, (err, result) => {
             if (err) throw err
+
+            if (result.length >= 2 && result[1] != null){
+                let count = result[1].length || 0
+
+                let query_store = "UPDATE store SET like_count = ? WHERE store_id = ?;"
+                query_store = mysql.format(query_store, [count, placeId])
+
+                req.app.get('dbConnection').query(query_store, (err, result) => {if (err) throw err})
+            }
 
             return res.json({
                 "placeId": placeId
@@ -61,12 +70,21 @@ const likePlace = {
         let query_like = "DELETE FROM like_store WHERE user_id = ? AND store_id = ?;"
         query_like = mysql.format(query_like, [userId, placeId])
 
-        let query_store = "UPDATE store SET like_count = like_count - 1  WHERE store_id = ?;"
-        query_store = mysql.format(query_store, placeId)
+        let query_like2 = "SELECT * FROM like_store WHERE store_id = ?;"
+        query_like2 = mysql.format(query_like2, placeId)
 
         // DB 요청
-        req.app.get('dbConnection').query(query_like + query_store, (err, result) => {
+        req.app.get('dbConnection').query(query_like + query_like2, (err, result) => {
             if (err) throw err
+
+            if (result.length >= 2 && result[1] != null){
+                let count = result[1].length || 0
+
+                let query_store = "UPDATE store SET like_count = ? WHERE store_id = ?;"
+                query_store = mysql.format(query_store, [count, placeId])
+
+                req.app.get('dbConnection').query(query_store, (err, result) => {if (err) throw err})
+            }
 
             return res.json({
                 "placeId": placeId
--- src/routes/PlaceReview/placeReview.ctrl.js
+++ src/routes/PlaceReview/placeReview.ctrl.js
@@ -4,7 +4,7 @@ const placeReview = {
     post: (req, res) => {
         // 유저, 장소, 코멘트 데이터를 얻어온다
         const userId = req.body.userId
-        const storeId = req.body.placeId
+        const placeId = req.body.placeId
         const comment = req.body.comment
 
         // 현재 시간을 생성한다
@@ -14,10 +14,10 @@ const placeReview = {
 
         // 쿼리 생성
         let query_review = "INSERT INTO review (store_id, comment, writing_time, user_id) VALUES (?, ?, ?, ?);"
-        query_review = mysql.format(query_review, [storeId, comment, now, userId])
+        query_review = mysql.format(query_review, [placeId, comment, now, userId])
 
         let query_review2 = "SELECT * FROM review WHERE store_id = ?"
-        query_review2 = mysql.format(query_review2, storeId)
+        query_review2 = mysql.format(query_review2, placeId)
 
         // DB 요청 및 리턴
         req.app.get('dbConnection').query(query_review + query_review2, (err, result) => {
@@ -27,7 +27,7 @@ const placeReview = {
                 let count = result[1].length || 0
 
                 let query_store = "UPDATE store SET review_count = ? WHERE store_id = ?;"
-                query_store = mysql.format(query_store, [count, storeId])
+                query_store = mysql.format(query_store, [count, placeId])
 
                 req.app.get('dbConnection').query(query_store, (err, result) => {if (err) throw err})
             }
