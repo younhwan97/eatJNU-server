8e1367647be8838f064dee3345c0939a87ee9c12
younhwan97 <younhwan0903@gmail.com>
2023-03-08 01:14:21 +0900

    [Refactor] .

--- src/routes/PlaceReview/placeReview.ctrl.js
+++ src/routes/PlaceReview/placeReview.ctrl.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql')
+
 const placeReview = {
     post: (req, res) => {
         // 유저, 장소, 코멘트 데이터를 얻어온다
@@ -11,10 +13,14 @@ const placeReview = {
         const now = today.toISOString().replace('T', ' ').substring(0, 19);
 
         // 쿼리 생성
-        const query = "INSERT INTO review (store_id, comment, writing_time, user_id) VALUES (?, ?, ?, ?);"
+        let query_review = "INSERT INTO review (store_id, comment, writing_time, user_id) VALUES (?, ?, ?, ?);"
+        query_review = mysql.format(query_review, [storeId, comment, now, userId])
+
+        let query_store = "UPDATE store SET review_count = review_count + 1  WHERE store_id = ?;"
+        query_store = mysql.format(query_store, storeId)
 
         // DB 요청 및 리턴
-        req.app.get('dbConnection').query(query, [storeId, comment, now, userId], (err, result) => {
+        req.app.get('dbConnection').query(query_review + query_store, (err, result) => {
             if (err) throw err
 
             return res.json({
