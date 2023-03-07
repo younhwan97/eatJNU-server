08cac8e34940990cb882fd61934f31ef40d85591
younhwan97 <younhwan0903@gmail.com>
2023-03-08 03:03:47 +0900

    [Refactor] .

--- src/routes/PlaceDetail/placeDetail.ctrl.js
+++ src/routes/PlaceDetail/placeDetail.ctrl.js
@@ -37,35 +37,36 @@ const placeDetail = {
         req.app.get('dbConnection').query(query_store + query_img + query_review, (err, result) => {
             if (err) throw err
 
+            // 결과 생성
             if (result.length !== 0 && result[0] != null && result[1] != null) {
                 const info = result[0][0]
 
-                ans["id"] = info.store_id || -1
-                ans["name"] = info.name || ""
-                ans["likeCount"] = info.like_count || 0
-                ans["reviewCount"] = info.review_count || 0
-                ans["filter"] = info.filter || ""
-                ans["tags"] = info.tags || ""
-                ans["image"] = info.url || ""
-                ans["location"] = info.location || ""
-                ans["number"] = info.number || ""
-                ans["openingInfo"] = info.opening_info || ""
-                ans["lat"] = info.lat || 0
-                ans["lon"] = info.lon || 0
+                ans["id"] = info.store_id ?? -1
+                ans["name"] = info.name ?? ""
+                ans["likeCount"] = info.like_count ?? 0
+                ans["reviewCount"] = info.review_count ?? 0
+                ans["filter"] = info.filter ?? ""
+                ans["tags"] = info.tags ?? ""
+                ans["image"] = info.url ?? ""
+                ans["location"] = info.location ?? ""
+                ans["number"] = info.number ?? ""
+                ans["openingInfo"] = info.opening_info ?? ""
+                ans["lat"] = info.lat ?? 0
+                ans["lon"] = info.lon ?? 0
 
                 for (let i = 0; i < result[1].length; i++) {
                     ans["images"].push({
-                            "url": result[1][i]["url"] || ""
+                            "url": result[1][i]["url"] ?? ""
                         }
                     )
                 }
 
                 for (let i = 0; i < result[2].length; i++) {
                     ans["reviews"].push({
-                        "comment": result[2][i]["comment"] || "",
-                        "writingTime": result[2][i]["writing_time"] || "",
-                        "userId": result[2][i]["user_id"] || "",
-                        "likeCount": result[2][i]["like_count"] || 0
+                        "comment": result[2][i]["comment"] ?? "",
+                        "writingTime": result[2][i]["writing_time"] ?? "",
+                        "userId": result[2][i]["user_id"] ?? "",
+                        "likeCount": result[2][i]["like_count"] ?? 0
                     })
                 }
             }
