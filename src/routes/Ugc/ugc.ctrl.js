cdff53559de35d4a57df3a55159b03c1d8231c4c
younhwan97 <younhwan0903@gmail.com>
2023-03-17 17:51:14 +0900

    [Refactor] .

--- src/routes/Ugc/ugc.ctrl.js
+++ src/routes/Ugc/ugc.ctrl.js
@@ -13,14 +13,23 @@ const ugc = {
         }
 
         // 쿼리 생성
-        let query = "SELECT agree FROM ugc WHERE userId = ?;"
+        let query = "SELECT agree FROM ugc WHERE user_id = ?;"
         query = mysql.format(query, [userId])
 
         // DB 요청
         req.app.get('dbConnection').query(query, (err, result) => {
             if (err) throw err
 
-            console.log(result)
+            if (result.length === 0) {
+                query = "INSERT IGNORE INTO ugc(user_id, agree) VALUES (?, ?);"
+                query = mysql.format(query, [userId, false])
+
+                return res.json({
+                    "isSuccess": false
+                })
+            } else {
+                console.log(result)
+            }
         })
     },
 
